73c3bcc23828948b1548a2f5bcc2ab3add4e33a1
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-20 13:59:21 +0600

    custom loader added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ function App() {
   return (
     <div className="main">
     {/* <button className="btn"></button> */}
-    <div className="box">Box</div>
+    {/* <div className="box">Box</div> */}
+    <div className="loading">
+      <div className="glass"></div>
+      <div className="box-1">1</div>
+      <div className="box-2">2</div>
+      <div className="box-3">3</div>
+      <div className="box-4">4</div>
+      <div className="box-5">5</div>
+      <div className="box-6">6</div>
+      <div className="box-7">7</div>
+      <div className="box-8">8</div>
+    </div>
     </div>
   );
 }
