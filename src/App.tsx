418c47d0cbe521212bdf26a3f662a114327df296
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-01-21 12:57:16 +0600

    text animation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import 'animate.css';
 
 function App() {
+  const name = 'Programming Hero';
+  const nameArr = name.split('');
+  
   return (
     <div className="main">
-    {/* <button className="btn"></button> */}
-    {/* <div className="box">Box</div> */}
-    <div className="loading">
+        {/* <button className="btn"></button>  */}
+     {/* <div className="box">Box</div>  */}
+     {/* <div className="loading">
       <div className="glass"></div>
       <div className="box-1">1</div>
       <div className="box-2">2</div>
@@ -15,7 +19,22 @@ function App() {
       <div className="box-6">6</div>
       <div className="box-7">7</div>
       <div className="box-8">8</div>
+    </div>  */}
+    {/* <div className="card-parent ">
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, minus eaque? Aut enim quisquam blanditiis ratione doloribus reprehenderit quos ut.</h3>
+    <div className="card">
+      <div className="card-child">
+        <h1>Header</h1>
+        <p className="what">??</p>
+      </div>
+<h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur ullam debitis aliquam voluptates delectus possimus fuga dignissimos dolore saepe tempora!</h4>
     </div>
+    </div> */}
+      <div className="container">
+        {nameArr.map((letter, i) => (
+          <span className="letter" style={{ transitionDelay: `${i * 50}ms` }}>{letter}</span>
+        ))}
+      </div>
     </div>
   );
 }
